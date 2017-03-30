9316448de78e1df3c7530483fafcf62ba869c0a2
LauraGwilliams <lgwilliams90@gmail.com>
2017-03-30 11:50:50 -0400

    finished working version of TimeFreq Class

--- mne/decoding/_time_frequency_csp.py
+++ mne/decoding/_time_frequency_csp.py
@@ -15,36 +15,19 @@
 
 import numpy as np
 
-# from .base import _set_cv
-
-from mne.filter import band_pass_filter
+from mne.filter import filter_data
 from mne.decoding import CSP
-
-
-# add imports in object's method, remove noqa
-from sklearn.lda import LDA  # noqa
-from sklearn.cross_validation import ShuffleSplit  # noqa
-from sklearn.pipeline import Pipeline  # noqa
-from sklearn.cross_validation import cross_val_score  # noqa
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.model_selection import KFold
-from sklearn.pipeline import make_pipeline
 
 
 class TimeFrequencyCSP(object):
 
-    def __init__(self, tmin, tmax, min_freq, max_freq, freq_bins, cv,
-                 estimator, n_cycles=7., sfreq=None, scorer=None, n_jobs=1):
+    def __init__(self, tmin, tmax, freqs, estimator, n_cycles=7., sfreq=None,
+                 scorer=None, n_jobs=1):
 
-        # replace min_freq, max_freq and freqs_bin into freqs
-        # remove cv
+        # add test that the number of cycles is not too many for the low freq
+
         # add input testing (e.g. mne.decoding.csp.CSP)
-        self.tmin = tmin
-        self.tmax = tmax
-        self.min_freq = min_freq
-        self.max_freq = max_freq
-        self.freq_bins = freq_bins
-        self.cv = cv
+        self.freqs = freqs
         self.estimator = estimator
         self.n_cycles = n_cycles
         self.sfreq = sfreq
@@ -52,75 +35,45 @@ class TimeFrequencyCSP(object):
         self.n_jobs = n_jobs
 
         # Assemble list of frequency range tuples
-        self.freqs = np.linspace(min_freq, max_freq, freq_bins)
-        self.freq_ranges = zip(self.freqs[:-1], self.freqs[1:])
-
-        # if constructed parameters they must be hidden using a _ before
+        self._freq_ranges = zip(self.freqs[:-1], freqs[1:])
 
         # Infer window spacing from the max freq and n cycles to avoid gaps
-        self.window_spacing = (n_cycles / np.max(self.freqs) / 2.)
-        self.centered_w_times = np.arange(tmin, tmax, self.window_spacing)[1:]
-        self.n_windows = len(self.centered_w_times)
+        self._window_spacing = (n_cycles / np.max(freqs) / 2.)
+        self._centered_w_times = np.arange(tmin+self._window_spacing,
+                                           tmax-self._window_spacing,
+                                           self._window_spacing)[1:]  # noqa
+        self._n_windows = len(self._centered_w_times)
 
-    def _transform(self, epochs, fmin, fmax, w_tmin, w_tmax):
+    def _transform(self, epochs, y, w_tmin, w_tmax, method='', pos=[]):
         """
         Assumes data for one frequency band for one time window and fits CSP.
         """
 
-        # filter the data at the desired frequency
-        epochs._data = band_pass_filter(epochs.get_data(), self.sfreq, fmin,
-                                        fmax)
+        from sklearn import clone
 
         # Crop data into time-window of interest
         Xt = epochs.copy().crop(w_tmin, w_tmax).get_data()
 
-        return Xt
+        # call fit or predict depending on calling function
+        if method == 'fit':
+            self.estimator = clone(self.estimator)
+            self._estimators[pos] = (self.estimator.fit(Xt, y))
+            return self
 
-    def _loop_tf(self, epochs, y=None, method=None):
+        elif method == 'predict':
+            self.y_pred = self._estimators[pos].predict(Xt)
 
-        flen = len(self.freq_ranges)  # change to n_freqs
-        tlen = len(self.centered_w_times)  # n_windows
-        n_splits = self.cv.n_splits
-
-        self._csps = []
-        self._scores = np.zeros([flen, tlen, n_splits])
-
-        for split_idx, (train, test) in enumerate(self.cv_splits):
-
-            for freq_ii, (fmin, fmax) in enumerate(self.freq_ranges):
-                # Infer window size based on the frequency being used
-                w_size = n_cycles / ((fmax + fmin) / 2.)  # in seconds
-
-                # Roll covariance, csp and lda over time
-                for t, w_time in enumerate(self.centered_w_times):
-
-                    # Center the min and max of the window
-                    w_tmin = w_time - w_size / 2.
-                    w_tmax = w_time + w_size / 2.
-
-                    # extract data for this frequency band and this time window
-                    Xt = self._transform(epochs, fmin, fmax, w_tmin, w_tmax)
-
-                    # call fit or predict depending on calling function
-                    if method == 'fit':
-                        self._fit_slice(Xt[train], y[train])
-                    elif method == 'predict':
-                        self._predict_slice(Xt[test])
-                    elif method == 'score':
-                        self._predict_slice(Xt[test])
-                        self._score_slice(Xt[test], y[test],
-                                         (freq_ii, t, split_idx))
-
-        return self
-
-    def _fit_slice(self, Xt, y):
-
-        _csp = self.estimator.fit(Xt, y)
-        self._csps.append(_csp)
-
-        return self
+        elif method == 'score':
+            self.y_pred = self._estimators[pos].predict(Xt)
+            print self.y_pred
+            return self._estimators[pos].score(Xt, y)
 
     def fit(self, epochs, y):
+
+        n_freq = len(self._freq_ranges)
+        n_window = len(self._centered_w_times)
+        self._scores = np.zeros([n_freq, n_window])
+        self._estimators = np.empty([n_freq, n_window], dtype=object)
 
         # TODO: change the work flow such that the bandpass is only done once
         # per frequency. this means moving the freq loop and the window loop
@@ -130,34 +83,79 @@ class TimeFrequencyCSP(object):
         # the estimators for each window and freq. if it is predict we need to
         # return y_pred to compare to y_true at the scoring stage.
 
-        self.cv_splits = cv.split(epochs.get_data())
+        # all of the cross validation is done outside of the functions
 
-        return self._loop_tf(epochs, y, method='fit')
+        for freq_ii, (fmin, fmax) in enumerate(self._freq_ranges):
+            # Infer window size based on the frequency being used
+            w_size = n_cycles / ((fmax + fmin) / 2.)  # in seconds
 
-    def _predict_slice(self, Xt):
+            # filter the data at the desired frequency
+            epochs_bp = epochs.copy()
+            epochs_bp._data = filter_data(epochs_bp.get_data(),
+                                            self.sfreq, fmin, fmax)
 
-        _csp = self.estimator.predict(Xt)
-        self._csps.append(_csp)
+            # Roll covariance, csp and lda over time
+            for t, w_time in enumerate(self._centered_w_times):
 
-        return self
+                # Center the min and max of the window
+                w_tmin = w_time - w_size / 2.
+                w_tmax = w_time + w_size / 2.
+
+                # extract data for this time window
+                self._transform(epochs_bp, y, w_tmin, w_tmax, method='fit',
+                                pos=(freq_ii, t))
+        pass
 
     def predict(self, epochs):
 
-        return self._loop_tf(epochs, method='predict')
+        for freq_ii, (fmin, fmax) in enumerate(self._freq_ranges):
+            # Infer window size based on the frequency being used
+            w_size = n_cycles / ((fmax + fmin) / 2.)  # in seconds
 
-    def _score_slice(self, Xt, y, pos=[]):
+            # filter the data at the desired frequency
+            epochs_bp = epochs.copy()
+            epochs_bp._data = filter_data(epochs_bp.get_data(),
+                                            self.sfreq, fmin, fmax)
 
-        scores = self.estimator.score(Xt, y)
-        self._scores[pos] = (scores)
+            # Roll covariance, csp and lda over time
+            for t, w_time in enumerate(self._centered_w_times):
 
-        return scores
+                # Center the min and max of the window
+                w_tmin = w_time - w_size / 2.
+                w_tmax = w_time + w_size / 2.
+
+                # extract data for this time window
+                self._transform(epochs_bp, None, w_tmin, w_tmax,
+                                method='predict', pos=(freq_ii, t))
+        pass
 
     def score(self, epochs, y):
 
-        scores = self._loop_tf(epochs, y, method='score')
-        scores = np.mean(scores, axis=-1)  # remove because cv outside
+        for freq_ii, (fmin, fmax) in enumerate(self._freq_ranges):
+            # Infer window size based on the frequency being used
+            w_size = n_cycles / ((fmax + fmin) / 2.)  # in seconds
 
-        return scores
+            # filter the data at the desired frequency
+            epochs_bp = epochs.copy()
+            epochs_bp._data = filter_data(epochs_bp.get_data(),
+                                            self.sfreq, fmin, fmax)
+
+            # Roll covariance, csp and lda over time
+            for t, w_time in enumerate(self._centered_w_times):
+
+                # Center the min and max of the window
+                w_tmin = w_time - w_size / 2.
+                w_tmax = w_time + w_size / 2.
+
+                # extract data for this time window
+                s = self._transform(epochs_bp, y, w_tmin, w_tmax,
+                                    method='score', pos=(freq_ii, t))
+                self._scores[freq_ii, t] = s
+
+        return self._scores
+
+
+
 
 
 # Load data
@@ -166,6 +164,12 @@ class TimeFrequencyCSP(object):
 from mne import Epochs, find_events
 from mne.io import concatenate_raws, read_raw_edf
 from mne.datasets import eegbci
+
+# add imports in object's method, remove noqa
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.pipeline import Pipeline  # noqa
+from sklearn.model_selection import KFold
+from sklearn.pipeline import make_pipeline
 
 event_id = dict(hands=2, feet=3)  # motor imagery: hands vs feet
 subject = 1
@@ -185,18 +189,36 @@ estimator = make_pipeline(CSP(n_components=4, reg=None, log=True),
 n_splits = 5  # how many folds to use for cross-validation
 cv = KFold(n_splits=n_splits, shuffle=True)
 
-tmin, tmax = -.500, 1.000
-n_cycles = 4.  # how many complete cycles: used to define window size
+# Classification & Time-frequency parameters
+tmin, tmax = -.200, 2.000
+n_cycles = 10.  # how many complete cycles: used to define window size
 min_freq = 5.
 max_freq = 25.
-freq_bins = 4  # how many frequency bins to use
+n_freqs = 8  # how many frequency bins to use
 
-epochs = Epochs(raw, events, event_id, tmin, tmax,
+epochs = Epochs(raw, events, event_id, tmin * 10, tmax * 10,
                     proj=False, baseline=None, add_eeg_ref=False, preload=True)
 y = epochs.events[:, 2] - 2
 sfreq = epochs.info['sfreq']
 
-tf = TimeFrequencyCSP(tmin, tmax, min_freq, max_freq, freq_bins, cv, estimator,
+freqs = np.linspace(min_freq, max_freq, n_freqs)
+tf = TimeFrequencyCSP(tmin, tmax, freqs, estimator,
                       sfreq=sfreq)
-tf.fit(epochs, y)
-scores = tf.score(epochs, y)
+
+scores = []
+for train, test in cv.split(epochs.copy().get_data()):
+    tf.fit(epochs[train], y[train])
+    scores.append(tf.score(epochs[test], y[test]))
+ave_scores = np.mean(np.array(scores), axis=0)
+
+# Set up time frequency object
+import matplotlib.pyplot as plt
+from mne.time_frequency import AverageTFR
+from mne import create_info
+
+av_tfr = AverageTFR(create_info(['freq'], sfreq), ave_scores[np.newaxis, :],
+                    tf._centered_w_times, freqs[1:], 1)
+
+chance = np.mean(y)  # set chance level to white in the plot
+av_tfr.plot([0], vmin=chance, title="Time-Frequency Decoding Scores",
+            cmap=plt.cm.Reds)
